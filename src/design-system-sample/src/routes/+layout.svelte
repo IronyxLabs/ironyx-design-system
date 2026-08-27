d34347129ed81c79e5678d29b2775cbafa31b141
Mihucza Bence <mihben@gmail.com>
2026-08-27 09:17:59 +0200

    feat: Added color and typography tokens

--- src/design-system-sample/src/routes/+layout.svelte
+++ src/design-system-sample/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+	
 </script>
 
 <style lang="scss">
@@ -11,6 +13,9 @@
     }
     
         .header {
+            display: grid;
+            grid-template-rows: 1fr auto;
+        
             font-size: 2rem;
         
             align-content: center;
@@ -19,10 +24,40 @@
             border-bottom-color: #454d53;
             border-bottom-style: solid;
             border-bottom-width: 1px;
+
+            padding: .4rem;
         }
     
         .content {
             padding: .4rem;
+        }
+
+    nav.menu {
+        display: grid;
+        grid-template-columns: auto auto auto 1fr;
+        gap: .8rem;
+    }
+
+    a.menu_item {
+        font-size: 1rem;
+
+        cursor: pointer;
+
+        padding: .2rem;
+        border-radius: 4px;
+        
+        color: #e8eaec;
+
+        text-decoration: none;
+    }
+
+        a.menu_item:hover {
+            color: #b4babf;
+        }
+    
+        a.menu_item[selected] {
+            background-color: #e9571c;
+            color: #090b0d;
         }
 </style>
 
@@ -36,6 +71,11 @@
 <div class="main">  
   <div class="header">
       <span>Ironyx Design System</span>
+      <nav class="menu">
+          <a class="menu_item" selected={$page.url.pathname === '/primitive'} href="/primitive">Primitive</a>
+          <a class="menu_item" selected={$page.url.pathname === '/semantic'} href="/semantic">Semantic</a>
+          <a class="menu_item" selected={$page.url.pathname === '/components'} href="/components">Components</a>
+      </nav>
   </div>  
 
   <div class="content">
