bec9d5333218ea7b52ca7d4dded95d93b2e37336
Bence Mihucza <mihben@gmail.com>
2026-09-17 16:10:17 +0200

    refactor: Move primitive, semantics and basic style to library

--- src/design-system-sample/src/routes/+layout.svelte
+++ src/design-system-sample/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import favicon from '$lib/assets/favicon.svg';
+	import '@ironyx/design-system/styles';
 
 	let { children } = $props();
 	
