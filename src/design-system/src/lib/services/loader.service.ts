808af1ec2b6f114160da3e171cc13c526321a8ae
Mihucza Bence <mihben@gmail.com>
2026-09-26 00:38:26 +0200

    chore: Formatting

--- src/design-system/src/lib/services/loader.service.ts
+++ src/design-system/src/lib/services/loader.service.ts
@@ -1,23 +1,23 @@
-import { loaderStore } from "./loader.store.ts";
+import { loaderStore } from './loader.store.ts';
 
 export class LoaderService {
-  public show(key: string = '') {
-    const loader = loaderStore.provide(key);
-    loader.activate();
-  }
+	public show(key: string = '') {
+		const loader = loaderStore.provide(key);
+		loader.activate();
+	}
 
-  public hide(key: string = '') {
-    const loader = loaderStore.provide(key);
-    loader.deactivate();
-  }
+	public hide(key: string = '') {
+		const loader = loaderStore.provide(key);
+		loader.deactivate();
+	}
 
-  public async loadAsync<T>(action: () => T | Promise<T>, key: string = '') {
-    try {
-      loaderStore.provide(key).activate();
-      
-      return await action();
-    } finally {
-      loaderStore.provide(key).deactivate();
-    }
-  }
+	public async loadAsync<T>(action: () => T | Promise<T>, key: string = '') {
+		try {
+			loaderStore.provide(key).activate();
+
+			return await action();
+		} finally {
+			loaderStore.provide(key).deactivate();
+		}
+	}
 }
--- src/design-system/src/lib/services/loader.test.ts
+++ src/design-system/src/lib/services/loader.test.ts
@@ -1,44 +1,44 @@
-import { describe, expect, it } from "vitest";
-import { Loader } from "./loader.svelte.ts";
+import { describe, expect, it } from 'vitest';
+import { Loader } from './loader.svelte.ts';
 
 describe('LDR - Loader', () => {
-  const createSUT = () => new Loader();
-  
-  it('[UNIT][LDR-001]: Activate', () => {
-    // Arrange
-    const sut = createSUT();
-    
-    // Act
-    sut.activate();
-     
-    // Assert
-    expect(sut.isActive).toBeTruthy();
-  })
-  
-  it('[UNIT][LDR-002]: Deactivate', () => {
-    // Arrange
-    const sut = createSUT();
-    
-    sut.activate();
-    
-    // Act
-    sut.deactivate();
-     
-    // Assert
-    expect(sut.isActive).toBeFalsy();
-  })
-  
-  it('[UNIT][LDR-003]: Activate Multiple Times', () => {
-    // Arrange
-    const sut = createSUT();
-    
-    sut.activate();
-    sut.activate();
-    
-    // Act
-    sut.deactivate();
-     
-    // Assert
-    expect(sut.isActive).toBeTruthy();
-  })
-})
+	const createSUT = () => new Loader();
+
+	it('[UNIT][LDR-001]: Activate', () => {
+		// Arrange
+		const sut = createSUT();
+
+		// Act
+		sut.activate();
+
+		// Assert
+		expect(sut.isActive).toBeTruthy();
+	});
+
+	it('[UNIT][LDR-002]: Deactivate', () => {
+		// Arrange
+		const sut = createSUT();
+
+		sut.activate();
+
+		// Act
+		sut.deactivate();
+
+		// Assert
+		expect(sut.isActive).toBeFalsy();
+	});
+
+	it('[UNIT][LDR-003]: Activate Multiple Times', () => {
+		// Arrange
+		const sut = createSUT();
+
+		sut.activate();
+		sut.activate();
+
+		// Act
+		sut.deactivate();
+
+		// Assert
+		expect(sut.isActive).toBeTruthy();
+	});
+});
