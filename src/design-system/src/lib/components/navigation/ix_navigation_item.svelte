ae1e04b6a45e9617642d77e864bdf95669d36648
Mihucza Bence <mihben@gmail.com>
2026-09-25 21:56:51 +0200

    chore: Fix format

--- src/design-system/src/lib/components/navigation/ix_navigation_item.svelte
+++ src/design-system/src/lib/components/navigation/ix_navigation_item.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-	import { page } from "$app/state";
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
 
-    let { label, icon, path }: { label: string, icon: string, path: string } = $props();
+	let { label, icon, path }: { label: string; icon: string; path: string } = $props();
 
-    function isActive(): boolean {
-      console.log(page.url.pathname);
-      return page.url.pathname === path;
-    }
+	function isActive(): boolean {
+		console.log(page.url.pathname);
+		return page.url.pathname === path;
+	}
 </script>
 
 <div class="navigation_item" onclick={() => goto(path)} active={isActive()}>
-    <i class="{icon}"></i>
-    <span class="navigation_item__label">{label}</span>
+	<i class={icon}></i>
+	<span class="navigation_item__label">{label}</span>
 </div>
--- src/design-system/src/lib/components/navigation/ix_navigation_item.test.ts
+++ src/design-system/src/lib/components/navigation/ix_navigation_item.test.ts
@@ -1,39 +1,40 @@
-import { fireEvent, render } from "@testing-library/svelte";
-import IxNavigationItem from "./ix_navigation_item.svelte";
-import { faker } from "@faker-js/faker";
+import { fireEvent, render } from '@testing-library/svelte';
+import IxNavigationItem from './ix_navigation_item.svelte';
+import { faker } from '@faker-js/faker';
 import { goto } from '$app/navigation';
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from 'vitest';
 
 describe('NAV - IxNavigation', () => {
-  const createSUT = (label: string, icon: string, path: string) => render(IxNavigationItem, {
-    label: label,
-    icon: icon,
-    path: path
-  })
-  
-  vi.mock('$app/navigation', () => ({
-      goto: vi.fn()
-  }));
-  
-  it('[UNIT][NAV-001]: Show label', () => {
-    // Arrange
-    const label = faker.string.alphanumeric();
-    
-    // Act
-    const sut = createSUT(label, '', '');
-     
-    // Assert
-    expect(sut.getByText(label)).toBeInTheDocument();
-  })
-  
-  it('[UNIT][NAV-002]: Show icon', () => {
-    // Arrange
-    const icon = faker.string.alphanumeric();
-    
-    // Act
-    const sut = createSUT('', icon, '');
-     
-    // Assert
-    expect(sut.container.querySelector('i')).toHaveClass(icon);
-  })
-})
+	const createSUT = (label: string, icon: string, path: string) =>
+		render(IxNavigationItem, {
+			label: label,
+			icon: icon,
+			path: path
+		});
+
+	vi.mock('$app/navigation', () => ({
+		goto: vi.fn()
+	}));
+
+	it('[UNIT][NAV-001]: Show label', () => {
+		// Arrange
+		const label = faker.string.alphanumeric();
+
+		// Act
+		const sut = createSUT(label, '', '');
+
+		// Assert
+		expect(sut.getByText(label)).toBeInTheDocument();
+	});
+
+	it('[UNIT][NAV-002]: Show icon', () => {
+		// Arrange
+		const icon = faker.string.alphanumeric();
+
+		// Act
+		const sut = createSUT('', icon, '');
+
+		// Assert
+		expect(sut.container.querySelector('i')).toHaveClass(icon);
+	});
+});
