f2519fb947d937c8e61e90d42d94f6d61b41462c
Mihucza Bence <mihben@gmail.com>
2026-09-26 22:01:25 +0200

    ci: Fix test

--- src/design-system/src/lib/components/loading-spinner/ix-loading-spinner.test.ts
+++ src/design-system/src/lib/components/loading-spinner/ix-loading-spinner.test.ts
@@ -5,31 +5,34 @@ import { loaderStore } from '$lib/services/loader.store.js';
 import { faker } from '@faker-js/faker';
 
 describe('LSP - Loading Spinner', () => {
-	const createSUT = () => render(IxLoadingSpinner);
+  const createSUT = (key: string) => render(IxLoadingSpinner, { key: key });
 
-	it('[UNIT][LSP-001]: Show Loading Spinner', () => {
-		// Act
-		const key = faker.string.alpha();
-		const sut = createSUT();
+  it('[UNIT][LSP-001]: Show Loading Spinner', () => {
+    // Act
+    const key = faker.string.alpha();
+    const sut = createSUT(key);
 
-		// Arrange
-		loaderStore.provide(key).activate();
+    // Arrange
+    loaderStore.provide(key).activate();
+    sut.rerender({});
 
-		// Assert
-		expect(sut.container.querySelector('.loader')).toBeInTheDocument();
-	});
+    // Assert
+    expect(sut.container.querySelector('.loader')).toBeInTheDocument();
+  });
 
-	it('[UNIT][LSP-002]: Hide Loading Spinner', () => {
-		// Act
-		const key = faker.string.alpha();
-		const sut = createSUT();
+  it('[UNIT][LSP-002]: Hide Loading Spinner', () => {
+    // Act
+    const key = faker.string.alpha();
+    const sut = createSUT(key);
 
-		loaderStore.provide(key).activate();
+    loaderStore.provide(key).activate();
+    sut.rerender({});
 
-		// Arrange
-		loaderStore.provide(key).deactivate();
+    // Arrange
+    loaderStore.provide(key).deactivate();
+    sut.rerender({});
 
-		// Assert
-		expect(sut.container.querySelector('.loader')).not.toBeInTheDocument();
-	});
+    // Assert
+    expect(sut.container.querySelector('.loader')).not.toBeInTheDocument();
+  });
 });
